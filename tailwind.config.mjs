158bd96bcacb44c04a36ca0dc1f9df24f4ed7073
Fisher Sun <f.sun@wustl.edu>
2024-01-10 00:59:46 -0500

    Use Alegreya Sans font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,6 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+import plugin from "tailwindcss/plugin"
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -12,8 +15,20 @@ export default {
 				},
 			}
 		},
+		fontFamily: {
+			sans: ["Alegreya Sans", "Libre Franklin", ...defaultTheme.fontFamily.sans],
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
+		/*
+		Change the base font size because Alegreya is a bit smaller than the standard fonts (e.g. Arial)
+		https://design2tailwind.com/blog/change-tailwindcss-base-font-size/
+		 */
+		plugin(function({ addBase }) {
+			addBase({
+			   'html': {fontSize: "1.125rem" },
+			 })
+		   }),
 	],
 }
