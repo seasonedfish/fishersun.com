7a82b55f798c30b8bc9a392f3106dcaa5636e4ef
Fisher Sun <f.sun@wustl.edu>
2024-01-27 17:22:13 -0600

    Define own syntax theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
+import syntaxTheme from "./src/assets/syntax.json";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: syntaxTheme
+    }
+  }
 });
