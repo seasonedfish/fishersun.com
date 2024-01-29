b63b8feac17b78a22d22b252562d0dce7053f337
Fisher Sun <f.sun@wustl.edu>
2024-01-29 09:41:26 -0600

    Update compose

--- scripts/compose.js
+++ scripts/compose.js
@@ -2,16 +2,20 @@
  * Creates a new blog post
  */
 
-import fs from "node:fs"
+import fs from "node:fs";
 
+/**
+ * @param {string[]} args command-line arguments passed to the script
+ * @returns {number} the exit code
+ */
 function main(args) {
-    const POSTS_DIRECTORY = "./src/content/posts"
+    const POSTS_DIRECTORY = "./src/content/posts";
     if (!fs.existsSync(POSTS_DIRECTORY)) {
         console.error("Could not find posts directory. Must run script in project root");
         return 1;
     }
 
-    const title = args[0]
+    const title = args[0];
     if (!title) {
         console.error("Must pass name of post as cli argument");
         return 2;
@@ -25,7 +29,7 @@ description: ""
 author: "Fisher Sun"
 tags: []
 ---
-`
+`;
     fs.writeFileSync(`${POSTS_DIRECTORY}/${slug}.md`, contents);
     return 0;
 }
