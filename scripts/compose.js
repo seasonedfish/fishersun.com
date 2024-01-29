4af2d1c62cc00c8f0262e5e19b7da04d5625fc4e
Fisher Sun <f.sun@wustl.edu>
2024-01-28 18:34:47 -0600

    Update compose to error with error exit code

--- scripts/compose.js
+++ scripts/compose.js
@@ -7,14 +7,14 @@ import fs from "node:fs"
 function main(args) {
     const POSTS_DIRECTORY = "./src/content/posts"
     if (!fs.existsSync(POSTS_DIRECTORY)) {
-        console.log("Could not find posts directory. Must run script in project root");
-        return;
+        console.error("Could not find posts directory. Must run script in project root");
+        return 1;
     }
 
     const title = args[0]
     if (!title) {
-        console.log("Must pass name of post as cli argument");
-        return;
+        console.error("Must pass name of post as cli argument");
+        return 2;
     }
 
     const slug = title.toLowerCase().replace(/\s+/g, '-');
@@ -27,6 +27,7 @@ tags: []
 ---
 `
     fs.writeFileSync(`${POSTS_DIRECTORY}/${slug}.md`, contents);
+    return 0;
 }
 
-main(process.argv.slice(2));
+process.exitCode = main(process.argv.slice(2));
