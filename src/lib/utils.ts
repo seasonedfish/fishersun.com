4b5df13329cd3904dac807e4556e3c2209568274
Fisher Sun <f.sun@wustl.edu>
2025-04-22 17:10:14 -0500

    Add tags page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,29 @@ export async function getPostsInReverseChronologicalOrder() {
         .toSorted((a, b) => a.data.pubDate.toISOString().localeCompare(b.data.pubDate.toISOString()))
         .toReversed();
 }
+
+export async function getTags() {
+    const posts = await getCollection("posts");
+    const tags = new Set<string>();
+    for (const post of posts) {
+        for (const tag of post.data.tags) {
+            tags.add(tag);
+        }
+    }
+    return Array.from(tags);
+}
+
+export async function getTagCounts() {
+    const posts = await getCollection("posts");
+    const counts = new Map<string, number>();
+    for (const post of posts) {
+        for (const tag of post.data.tags) {
+            if (counts.has(tag)) {
+                counts.set(tag, counts.get(tag)! + 1);
+            } else {
+                counts.set(tag, 1);
+            }
+        }
+    }
+    return counts;
+}
