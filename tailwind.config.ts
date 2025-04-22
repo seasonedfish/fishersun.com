c12a545e387f82b8bdaf52c9dba4019af252fb1d
Fisher Sun <f.sun@wustl.edu>
2025-04-22 12:14:28 -0500

    Change fonts and colors
    Inspired by Alex Haydock's website,
    https://blog.infected.systems/

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,8 +39,6 @@ export default {
 			}),
 		},
 		fontFamily: {
-			sans: ["FiraGO", ...defaultTheme.fontFamily.sans],
-			heading: ["Bitter Pro", ...defaultTheme.fontFamily.serif],
 			mono: [...defaultTheme.fontFamily.mono]
 		},
 	},
